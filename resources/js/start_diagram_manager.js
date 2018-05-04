cb5f37fb0650c6eaa6f2799a2c5f7624f4a61b17
KevinA <KevinA1224@users.noreply.github.com>
2018-05-04 10:37:00 +0200

    color correction
    Changed colors of the outer ring

--- resources/js/start_diagram_manager.js
+++ resources/js/start_diagram_manager.js
@@ -17,7 +17,7 @@ FlyingEtiquette.StartDiagramManager = function (divEl, svgEl) {
     
     function createOuterRing(data) {
         var radius = Math.min(width, height) / 2,
-            color = d3.scaleOrdinal().range(["#1565C0", "#B71C1C", "#C62828", "#EF6C00", "#6A1B9A", "#7B1FA2", "#8E24AA", "#00838F", "#4E342E", "#5D4037", "#D32F2F", "#E53935", "#F44336", "#0097A7", "#00ACC1", "#6D4C41", "#795548", "#F57C00", "#FB8C00", "#1B5E20", "#2E7D32", "#1976D2", "#1E88E5", "#2196F3", "#42A5F5", "#64B5F6"]),
+            color = d3.scaleOrdinal().range(["#1565C0", "#B71C1C", "#C62828", "#EF6C00", "#6A1B9A", "#7B1FA2", "#8E24AA", "#00838F", "#9E9D24", "#AFB42B", "#D32F2F", "#E53935", "#F44336", "#0097A7", "#00ACC1", "#C0CA33", "#CDDC39", "#F57C00", "#FB8C00", "#1B5E20", "#2E7D32", "#1976D2", "#1E88E5", "#2196F3", "#42A5F5", "#64B5F6"]),
             arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 70),
             pie = d3.pie().sort(null).value(outerRingValue),
             svg = d3.select(svgEl).attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
