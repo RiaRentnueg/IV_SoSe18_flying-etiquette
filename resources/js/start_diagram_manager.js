c632641dc3ecb523cb3ccc3708f13b06e4ee1ec6
KevinA <KevinA1224@users.noreply.github.com>
2018-05-18 11:38:43 +0200

    color_correction
    the inner ring now has the same color for questions and their respective answers, the answers are separated by a white stroke line for each segment

--- resources/js/start_diagram_manager.js
+++ resources/js/start_diagram_manager.js
@@ -10,7 +10,9 @@ FlyingEtiquette.StartDiagramManager = function (divEl, svgEl) {
         outerRingValue = 26,
         tempData = [],
         currData = [],
-        container = {};
+        container = {},
+        currQuestion = null,
+        colorRange = ["#1565C0", "#B71C1C", "#C62828", "#EF6C00", "#6A1B9A", "#7B1FA2", "#8E24AA", "#00838F", "#9E9D24", "#AFB42B", "#D32F2F", "#E53935", "#F44336", "#0097A7", "#00ACC1", "#C0CA33", "#CDDC39", "#F57C00", "#FB8C00", "#1B5E20", "#2E7D32", "#1976D2", "#1E88E5", "#2196F3", "#42A5F5", "#64B5F6"];
     
     function setupCsvData() {
         d3.csv("./data/flying-etiquette.csv", function(data) {
@@ -57,7 +59,7 @@ FlyingEtiquette.StartDiagramManager = function (divEl, svgEl) {
     
     function createOuterRing(data) {
         var radius = Math.min(width, height) / 2,
-            color = d3.scaleOrdinal().range(["#1565C0", "#B71C1C", "#C62828", "#EF6C00", "#6A1B9A", "#7B1FA2", "#8E24AA", "#00838F", "#9E9D24", "#AFB42B", "#D32F2F", "#E53935", "#F44336", "#0097A7", "#00ACC1", "#C0CA33", "#CDDC39", "#F57C00", "#FB8C00", "#1B5E20", "#2E7D32", "#1976D2", "#1E88E5", "#2196F3", "#42A5F5", "#64B5F6"]),
+            color = d3.scaleOrdinal().range(colorRange),
             arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 70),
             pie = d3.pie().sort(null).value(outerRingValue),
             svg = d3.select(svgEl).attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
@@ -69,13 +71,14 @@ FlyingEtiquette.StartDiagramManager = function (divEl, svgEl) {
     
     function createInnerRing(data) {
         var radius = Math.min(width/1.2, height/1.2) / 2,
-            color = d3.scaleOrdinal().range(["#1565C0", "#B71C1C", "#EF6C00", "#6A1B9A", "#00838F", "#9E9D24", "#1B5E20"]),
+            color = d3.scaleOrdinal().range(colorRange),
             arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 70),
             pie = d3.pie().sort(null).value(function(d){return d.value}),
             svg = d3.select(svgEl).attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
             g = svg.selectAll(".arc").data(pie(data.slice(1,data.length))).enter().append("g").attr("class", "arc").attr("id", function(d, i){return "innerRing" + i});
         
-        g.append("path").attr("d", arc).style("fill", function(d){return color(d.value)});
+        g.append("path").attr("d", arc).style("fill", function(d){return color(d.data["question"])}).style("stroke", "white").style("stroke-width", "0.5px");
+        //g.append("text").attr("transform", function(d){return "translate(" + arc.centroid(d) + ")"; }).attr("dy", ".35em").text(function(d){return d.data["question"]});
     }
     
     that.setupCsvData = setupCsvData;
