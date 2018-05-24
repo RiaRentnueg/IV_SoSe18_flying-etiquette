e271a40f5b53d49f3e30243036e495a99447269c
Lydia <lydia.guentner@web.de>
2018-05-24 19:04:35 +0200

    animate bubbles if size changes

--- resources/js/BubbleView.js
+++ resources/js/BubbleView.js
@@ -6,7 +6,9 @@ BubbleDiagram.BubbleView = function(params) {
 
   var that = new EventPublisher(),
     selector;
+  var transitionDelay = 1000;
 
+  var temp = 0;
 
   function init() {
     selector = params.selector;
@@ -14,7 +16,7 @@ BubbleDiagram.BubbleView = function(params) {
   }
 
   function setAnswersWithCount(answersWithCount){
-    d3.selectAll("svg > *").remove(); // will haunt us later!
+    //d3.selectAll("svg > *").remove(); // will haunt us later!
 
    var chartSVG = d3.select(selector)
    .attr("viewBox","0 0 960 960")
@@ -25,14 +27,21 @@ BubbleDiagram.BubbleView = function(params) {
 
    var bubbleNodes =  chartSVG.data(answersWithCount).enter().append("g");
 
+
    var rootNode = d3.hierarchy({children: answersWithCount})
    .sum(function(d) { return d.value; });
 
    d3.pack().padding(2).size([960,960])(rootNode);
 
    var rootNodeChildren = rootNode.children;
+   console.log("temp = " + temp);
+   if(temp == 0){
+     createBubbles(rootNodeChildren, bubbleNodes);
+     temp = 1;
+   }else {
+     updateBubbles(rootNodeChildren, bubbleNodes);
+   }
 
-   createBubbles(rootNodeChildren, bubbleNodes);
    addTextToBubbles(rootNodeChildren, bubbleNodes);
  }
 
@@ -61,8 +70,25 @@ BubbleDiagram.BubbleView = function(params) {
    }).style("text-anchor", "middle");
  }
 
+ function updateBubbles (answersWithCount, bubbleNodes) {
+   d3.selectAll(selector).selectAll("circle")
+    .data(answersWithCount)
+     .transition()
+     .duration(transitionDelay)
+   .style("fill", function() {
+     return "rgb(62,206,255)";
+   }).style("r", function (d){
+     return d.r;
+   }).attr("cx", function(d){
+     return d.x;
+   }).attr("cy", function(d){
+     return d.y;
+   });
+ }
+
 that.init = init;
 that.setAnswersWithCount = setAnswersWithCount;
+
 
 
 return that;
