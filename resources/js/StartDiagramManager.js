4d045557208fa5663ce6984e737963b78645548b
KevinA <KevinA1224@users.noreply.github.com>
2018-06-15 16:53:52 +0200

    show dots
    The dots for every entry in the dataset appear on the screen. They still have to be displayed differently, so that they all stay inside the inner ring

--- resources/js/StartDiagramManager.js
+++ resources/js/StartDiagramManager.js
@@ -17,6 +17,7 @@ FlyingEtiquette.StartDiagramManager = function (divEl, svgEl) {
         d3.csv("./data/flying-etiquette.csv", function() {
             createOuterRing(startDiagramModel.getOuterRingData());
             createInnerRing(startDiagramModel.getInnerRingData());
+            createDots(startDiagramModel.getDotsData());
         });
     }
     
@@ -29,7 +30,9 @@ FlyingEtiquette.StartDiagramManager = function (divEl, svgEl) {
             svg = d3.select(svgEl).attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
             g = svg.selectAll(".arc").data(pie(data)).enter().append("g").attr("class", "arc").attr("id", function(d, i){return "outerRing" + i});
         
-        g.append("path").attr("d", arc).style("fill", function(d){return color(d.data)});
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d){return color(d.data)});
         //g.append("text").attr("transform", function(d){return "translate(" + arc.centroid(d) + ")"; }).attr("dy", ".35em").text(function(d){return d.data});
     }
     
@@ -39,11 +42,35 @@ FlyingEtiquette.StartDiagramManager = function (divEl, svgEl) {
             color = d3.scaleOrdinal().range(colorRange),
             arc = d3.arc().outerRadius(radius - 10).innerRadius(radius - 70),
             pie = d3.pie().sort(null).value(function(d){return d.value}),
-            svg = d3.select(svgEl).attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
-            g = svg.selectAll(".arc").data(pie(data)).enter().append("g").attr("class", "arc").attr("id", function(d, i){return "innerRing" + i});
+            svg = d3.select(svgEl).attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
+            g = svg.selectAll(".arc")
+                .data(pie(data))
+                .enter()
+                .append("g")
+                .attr("class", "arc")
+                .attr("id", function(d, i){return "innerRing" + i});
         
-        g.append("path").attr("d", arc).style("fill", function(d){return color(d.data["question"])}).style("stroke", "white").style("stroke-width", "0.5px");
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d){return color(d.data["question"])})
+            .style("stroke", "white")
+            .style("stroke-width", "0.5px");
         //g.append("text").attr("transform", function(d){return "translate(" + arc.centroid(d) + ")"; }).attr("dy", ".35em").text(function(d){return d.data["question"]});
+    }
+    
+    function createDots(dotdata) {
+        var svg = d3.select(svgEl),
+            bubbles = svg.append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+                .selectAll(".bubble").data(dotdata)
+                .enter();
+        
+        bubbles.append("circle").attr("r", "5")
+            .style("fill", "#ff00ff")
+            .attr("transform", function(d,i){return "translate(" + (i*10) + "," + 0 + ")"});
     }
     
     that.setupStartDiagram = setupStartDiagram;
