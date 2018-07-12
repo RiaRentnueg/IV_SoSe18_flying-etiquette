278a14cff21b2f4453cfa15f236426368a3e65ba
KevinA <KevinA1224@users.noreply.github.com>
2018-07-12 22:17:16 +0200

    select multiple answers
    The user can now select multiple answers to change the color of the dots on the display. The active segments get a distinct color to show that they are active and the elements themselves are being saved in an array. If the user clicks on an active segment it will get back its original color and it is removed from the array of active elements. Some additional code comments were made and previous ones were improved or changed to better understand them. Right now the functionaltiy of selecting multiple answers only works with "OR" logic instead of "AND" logic. This will be implemented next.

--- resources/js/StartDiagramController.js
+++ resources/js/StartDiagramController.js
@@ -12,6 +12,7 @@ FlyingEtiquette.StartDiagramController = function() {
       innerInfoText,
       dotsInfoText,
       colorInformation = [],
+      activeSegments = [],
       colorRange = ["#1565C0", "#B71C1C", "#C62828", "#EF6C00", "#6A1B9A", "#7B1FA2", "#8E24AA", "#00838F", "#9E9D24", "#AFB42B", "#D32F2F", "#E53935", "#F44336", "#0097A7", "#00ACC1", "#C0CA33", "#CDDC39", "#F57C00", "#FB8C00", "#1B5E20", "#2E7D32", "#1976D2", "#1E88E5", "#2196F3", "#42A5F5", "#64B5F6"];
     
     function setupEventListeners() {
@@ -37,20 +38,33 @@ FlyingEtiquette.StartDiagramController = function() {
         for(let k = 1; k < dots.length; k++) {
             dots[k].addEventListener("click", showDotsInformation);
         }
-        
     }
     
     function changeDotsColor(e) {
-        var answer,
-            question,
-            keyPair;
+        var keyPair;
         
-        question = e.target["__data__"]["data"]["question"];
-        answer = e.target["__data__"]["data"]["answer"];
+        if(checkActiveOrInactive(e)){
+            activeSegments.push(e.target);
+        }
         
         resetDotsColor();
         resetInnerRingColor();
-        selectDotsColor(keyPair, answer, question);
+        markActiveSegments();
+        selectDotsColor(keyPair);
+                
+    }
+    
+    //checks if the clicked on segment is active or inactive, a segment is active after the user clicked on it and the user can set it as inactive by clicking on it again, if the user clicks on an element and it is included in the activeSegments array this segment is no longer active and then removed from the array
+    function checkActiveOrInactive(e){
+        var active = true;
+        for(let i = 0; i < activeSegments.length; i++) {
+            if(activeSegments[i] === e.target && activeSegments.length !== 0) {
+                activeSegments.splice(i, 1);
+                active = false;
+                break;
+            }
+        }
+        return active;
     }
     
     function resetDotsColor() {
@@ -77,13 +91,25 @@ FlyingEtiquette.StartDiagramController = function() {
         }
     }
     
-    //nur wenn Antwort und Frage übereinstimmen
-    function selectDotsColor(keyPair, answer, question) {
-        for(let i = 1; i < dots.length; i++) {
-            keyPair = dots[i]["__data__"]["data"];
-            for (let key in keyPair){
-                if (answer === keyPair[key] && key === question) {
-                    dots[i].style = ("fill: rgb(0,80,250)");
+    //currently active segments should be highlighted in a different color, the same color that is used for highlighting corresponding answers when clicking on a participant dot is also used here
+    function markActiveSegments() {
+        for(let i = 0; i < activeSegments.length; i++) {
+            activeSegments[i].style.fill = "rgb(0,0,80)";
+        }
+    }
+    
+    //only highlight a dot when the answer belongs to the right question, because there are multiple answers that consist of the same words but belong to different questions
+    function selectDotsColor(keyPair) {
+        for(let j = 0; j < activeSegments.length; j++) {
+        
+            for(let i = 1; i < dots.length; i++) {
+                keyPair = dots[i]["__data__"]["data"];
+                
+                //iterate through the data of the dot, e.g. the answers of the participant for every key (question)
+                for (let key in keyPair){
+                    if (activeSegments[j]["__data__"]["data"]["answer"] === keyPair[key] && key === activeSegments[j]["__data__"]["data"]["question"]) {
+                        dots[i].style = ("fill: rgb(0,80,250)");
+                    }
                 }
             }
         }
@@ -150,6 +176,7 @@ FlyingEtiquette.StartDiagramController = function() {
         showSelectedParticipant(e.target);
     }
     
+    //show only the participant dot that the user clicked on
     function showSelectedParticipant(test) {
         for(let i = 1; i < dots.length; i++) {
             if(dots[i] === test) {
